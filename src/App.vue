7c2a581d1b5d495fae18b3ef3e8b1dec16111c73
Arthur <arthurferreira1417@gmail.com>
2024-10-29 09:34:32 -0300

    Feat: start project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="tsx">
+let name = "Arthur";
+</script>
+
+<template>
+  <div  :class="$style.TextHead">
+    {{ name + "é foda" }}
+  </div>
+</template>
+<style lang="css" module>
+.TextHead {
+  font-size: 40px;
+  color: aqua;
+}
+</style>
