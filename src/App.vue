fe39dcd71ad397a6f9aac81a79dea1c462246ceb
Arthur <arthurferreira1417@gmail.com>
2024-10-29 09:40:33 -0300

    Feat: adding style to login page

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,26 @@ let name = "Arthur";
 </script>
 
 <template>
-  <div  :class="$style.TextHead">
-    {{ name + "é foda" }}
-  </div>
+  <main :class="$style.Main">
+    <aside :class="$style.Column">
+      ola
+    </aside>
+    <aside :class="$style.Column">
+      img
+    </aside>
+  </main>
 </template>
 <style lang="css" module>
-.TextHead {
-  font-size: 40px;
-  color: aqua;
+.Main {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  height: auto;
+}
+.Column{
+  width: 40%;
+  height: 100%;
 }
 </style>
